a3788e547b84b0905c87c006675e59e9afccdc4c
Vladislav Pereskokov <v-pereskokov@yandex-team.ru>
2020-05-02 12:45:32 +0300

    it works

---
+++ precache-manifest.feba6d5c6f1817efc22772d9e84b6e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8710a3bf5c6bd4891d259ab39c15b8a3",
+    "url": "/ya-praktikum-mid-frontend-lessons/index.html"
+  },
+  {
+    "revision": "3ade3803a2bce115639d",
+    "url": "/ya-praktikum-mid-frontend-lessons/static/css/main.baceee75.chunk.css"
+  },
+  {
+    "revision": "69d6ad3cf965dc845d26",
+    "url": "/ya-praktikum-mid-frontend-lessons/static/js/2.3ad7a45c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ya-praktikum-mid-frontend-lessons/static/js/2.3ad7a45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ade3803a2bce115639d",
+    "url": "/ya-praktikum-mid-frontend-lessons/static/js/main.3352c835.chunk.js"
+  },
+  {
+    "revision": "9f89496544124f02dbc8",
+    "url": "/ya-praktikum-mid-frontend-lessons/static/js/runtime-main.e7a95dc7.js"
+  }
+]);
