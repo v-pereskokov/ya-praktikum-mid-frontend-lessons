885c9de6531217a3ee625c7f399225656ed24740
Vladislav Pereskokov <v-pereskokov@yandex-team.ru>
2020-05-02 12:43:08 +0300

    fix

---
+++ precache-manifest.331c766b4196a255059b56204319d52e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bceb4e6203768425eb3020f8cd1eb00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ade3803a2bce115639d",
+    "url": "/static/css/main.baceee75.chunk.css"
+  },
+  {
+    "revision": "69d6ad3cf965dc845d26",
+    "url": "/static/js/2.3ad7a45c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ad7a45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ade3803a2bce115639d",
+    "url": "/static/js/main.3352c835.chunk.js"
+  },
+  {
+    "revision": "d1fd0f66f48f0592306c",
+    "url": "/static/js/runtime-main.f1d7e539.js"
+  }
+]);
